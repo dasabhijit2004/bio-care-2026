a33d9d55c2283d1d11a1e9d60bc3c2b7afd53137
Abhijit Das <sadabhijit2004@gmail.com>
2025-12-04 15:10:43 +0530

    Added Toast Message

--- bio-care/app/layout.tsx
+++ bio-care/app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import SiteHeader from "@/components/site-header";
 import SiteFooter from "@/components/site-footer";
+import { Toaster } from "sonner";
 
 export const metadata = {
   title: "Bio Care Coaching",
@@ -11,6 +12,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className="bg-background">
+
+        {/* Toaster for all notifications */}
+        <Toaster richColors closeButton position="top-right" />
+
         <SiteHeader />
         {children}
         <SiteFooter />
